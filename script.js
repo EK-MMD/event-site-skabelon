aa39f2a2986388fec3be1e90c38e1102cb4e29e0
daviatkea <54393843+daviatkea@users.noreply.github.com>
2026-08-10 23:16:53 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Interaktiv SVG-side
+const svgPlaceholder = document.querySelector(".svg-placeholder");
+
+// Opsamlingsside
+const opsamlingPlaceholder = document.querySelector(".opsamling-placeholder");
+
+if (opsamlingPlaceholder) {
+  const params = new URLSearchParams(window.location.search);
+  // Eksempel: params.get("navn") henter værdien fra et formularfelt med name="navn".
+}
